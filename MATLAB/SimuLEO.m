0b9c389dbf9a460dc0520e71cb5348cdb0aaeaff
AngelicaIseni <angelicaiseni21@gmail.com>
2024-04-27 15:36:21 +0200

    trying_mat_conn

--- MATLAB/SimuLEO.m
+++ MATLAB/SimuLEO.m
@@ -3,11 +3,11 @@
 % A.A. 2023/2024
 %
 % SimuLEO
-% 
+%
 % Angelica Iseni, Emma Lodetti
-% 
+%
 % References:
-% Positioning and Location Based Services Laboratory a.y. 2022/2023  
+% Positioning and Location Based Services Laboratory a.y. 2022/2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
@@ -19,8 +19,8 @@ set(0,'DefaultFigureWindowStyle','docked');
 %% Read data from txt files and compute position for each second in a day for each satellite
 
 % Define the folder path where your text files are located
-InputFolderPath = 'Almanacs060282';
-OutputFolderPath = 'SatellitePositions060282';
+InputFolderPath = 'C:\Documenti\GitHub\LEO_orbits_estimation\Almanacs040480';
+OutputFolderPath = 'C:\Documenti\GitHub\LEO_orbits_estimation\SatellitePositions040480';
 
 % List all files in the folder
 files = dir(fullfile(InputFolderPath, '*.txt'));
@@ -35,10 +35,10 @@ t = t_0:D_t:t_end;
 for i = 1:length(files)
     % Get the file name
     InputFileName = files(i).name;
-    
+
     % Create the full file path
     InputFilePath = fullfile(InputFolderPath, InputFileName);
-    
+
     % Read data
     [OrbitRadius,OrbitInclination,M0,Omega0] = ReadData(InputFilePath);
 
@@ -50,21 +50,12 @@ for i = 1:length(files)
 
 end
 
-%% Geoshow plot
-figure;
-ax = axesm ('eqdcylin', 'Frame', 'on', 'Grid', 'on', 'LabelUnits', 'degrees', 'MeridianLabel', 'on', 'ParallelLabel', 'on', 'MLabelParallel', 'south');
-geoshow('landareas.shp', 'FaceColor', 'black');
-hold on
-geoshow(ITRF_geod(:,1),ITRF_geod(:,2), 'DisplayType', 'point', 'MarkerEdgeColor', 'green');
-title('Groundtrack of satellite LEO0202');
-xlabel('longitude');
-ylabel('latitude');
 
 
 
 
 
 
-  
 
-    
+
+
